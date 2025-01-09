4a391bbabd8fda0c987d8ee5c7a4ecb423680198
Asif-Faizal <moh.asif@protonmail.ch>
2025-01-09 18:34:59 +0530

    Initial Commit

---
+++ Informed-iOS/Informed_iOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  Informed_iOSApp.swift
+//  Informed-iOS
+//
+//  Created by Innovitegra Solutions on 09/01/25.
+//
+
+import SwiftUI
+
+@main
+struct Informed_iOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
