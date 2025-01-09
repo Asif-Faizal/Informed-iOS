c66df16b637b070ed6963f2c3bfe2c3fd8f583fc
Asif-Faizal <moh.asif@protonmail.ch>
2025-01-09 18:34:59 +0530

    DELETED COMMANDS

---
+++ Informed-iOSTests/Informed_iOSTests.swift
@@ -0,0 +1,17 @@
+//
+//  Informed_iOSTests.swift
+//  Informed-iOSTests
+//
+//  Created by Innovitegra Solutions on 09/01/25.
+//
+
+import Testing
+@testable import Informed_iOS
+
+struct Informed_iOSTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
